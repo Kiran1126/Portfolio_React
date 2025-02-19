19bf3a5cf3ac45922aedf53f91b7bee80188448f
Kiran1126 <kiransamanta945@gmail.com>
2025-02-20 01:38:23 +0530

    Update Contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Heading from "../Heading/Heading";
 import BtnContainer from "../Container/BtnContainer";
 
@@ -30,20 +30,24 @@ const FormSection = () => (
     <div className="flex flex-col text-lg gap-3">
       <img className="w-full h-44 object-cover rounded-md cursor-not-allowed" src="https://i.pinimg.com/originals/9c/60/36/9c6036872d32db27c82c47c8a5657777.gif" alt="meme" />
       <div className="flex flex-col md:flex-row gap-5">
-        <input type="text" name="name" className="h-14 md:w-1/2 w-full rounded-md px-5 bg-slate-800 text-slate-200 focus:outline-blue-500" placeholder="Your name" />
-        <input type="email" name="email" className="h-14 md:w-1/2 w-full rounded-md px-5 bg-slate-800 text-slate-200 focus:outline-blue-500" placeholder="Email address" />
+        <input type="text" name="name" className="h-14 md:w-1/2 w-full rounded-md px-5 bg-slate-800 text-slate-200 focus:outline-blue-500" placeholder="Your name" required />
+        <input type="email" name="email" className="h-14 md:w-1/2 w-full rounded-md px-5 bg-slate-800 text-slate-200 focus:outline-blue-500" placeholder="Email address" required />
       </div>
-      <input type="text" name="subject" className="h-14 w-full rounded-md px-5 bg-slate-800 text-slate-200 focus:outline-blue-500" placeholder="Subject" />
-      <textarea name="message" className="h-40 p-5 w-full rounded-md px-5 bg-slate-800 text-slate-200 focus:outline-blue-500" placeholder="Message" />
+      <input type="text" name="subject" className="h-14 w-full rounded-md px-5 bg-slate-800 text-slate-200 focus:outline-blue-500" placeholder="Subject" required />
+      <textarea name="message" className="h-40 p-5 w-full rounded-md px-5 bg-slate-800 text-slate-200 focus:outline-blue-500" placeholder="Message" required />
       <BtnSection />
     </div>
   </form>
 );
 
+const [sentMsg, setSentMsg] = useState(false);
+
 const BtnSection = () => (
   <div className="animate-pulse">
-    <BtnContainer>
-      <p className="text-lg mr-1">Send</p> 
+    <BtnContainer onClick={() => setSentMsg(true)}>
+      <p className="text-lg mr-1">
+        {sentMsg ? "Message Sent" : "Send"}
+      </p> 
       <span className="material-symbols-outlined">send</span>
     </BtnContainer>
   </div>
